b0072f9e6370eb541a5543f102920934adc1456d
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-08-15 23:56:41 +0530

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-yellow-500">
+      <Text className="text-4xl text-red-600">
+        Hey its Android 15 emulator and this is updated
+      </Text>
+    </View>
+  );
+}
